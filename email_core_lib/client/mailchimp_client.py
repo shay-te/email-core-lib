f376455131968aa2c1dfe9297322343afa34fbca
shay-te <shay.te@gmail.com>
2022-09-15 07:22:55 +0300

    fix parameters and add log messages

--- email_core_lib/client/mailchimp_client.py
+++ email_core_lib/client/mailchimp_client.py
@@ -1,24 +1,44 @@
+import logging
+
 import mailchimp_transactional as MailchimpTransactional
 from mailchimp_transactional.api_client import ApiClientError
 
 
 class MailChimpClient:
     def __init__(self, api_key: str):
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.api_key = api_key
         self.mailchimp = MailchimpTransactional.Client(self.api_key)
 
     def send(self, template_name: str, params: dict):
+        template_content = []
+        for key, val in params.items():
+            if key != 'email':
+                template_content.append({
+                    'name': key,
+                    'content': str(val)
+                })
         message = {
             "to": [
                 {
                     "email": params['email'],
                     "type": "to"
                 }
-            ]
+            ],
+            "merge_language": "handlebars",
+            "merge": True,
+            "global_merge_vars": template_content,
         }
         try:
-            self.mailchimp.messages.send_template(
-                {"template_name": template_name, "template_content": [params], "message": message}
+            response = self.mailchimp.messages.send_template(
+                {"template_name": template_name, "template_content": template_content, "message": message}
             )
+            if response and len(response) > 0:
+                return True if response[0].get('status') == 'sent' else False
+            return False
         except ApiClientError as error:
-            print("An exception occurred: {}".format(error.text))
+            self.logger.error(f'An exception occurred: {error.text}')
+            self.logger.error(error)
+        except Exception as error:
+            self.logger.error(error)
+        return False
--- email_core_lib/email_core_lib.py
+++ email_core_lib/email_core_lib.py
@@ -1,3 +1,5 @@
+import logging
+
 from omegaconf import DictConfig
 from core_lib.core_lib import CoreLib
 from core_lib.helpers.config_instances import instantiate_config
@@ -6,9 +8,10 @@ from core_lib.helpers.config_instances import instantiate_config
 class EmailCoreLib(CoreLib):
     def __init__(self, conf: DictConfig):
         super().__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.config = conf
         self.mailchimp = instantiate_config(self.config.core_lib.email_core_lib.client)
 
     def send(self, template_id, params):
+        self.logger.info(f'send email. template_id: {template_id}, params: {params}')
         self.mailchimp.send(template_id, params)
-
